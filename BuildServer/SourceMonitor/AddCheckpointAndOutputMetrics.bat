04a57f33ae092bb7a2c3bb197c6afc1327886b95
Byran Wills-Heath <byran@bluefruit.co.uk>
2015-11-12 10:42:43 +0000

    Made the build scirpt build the solution

--- BuildServer/SourceMonitor/AddCheckpointAndOutputMetrics.bat
+++ BuildServer/SourceMonitor/AddCheckpointAndOutputMetrics.bat
@@ -1,16 +1,5 @@
 @echo off
 
-IF DEFINED ProgramFiles(x86) GOTO :64Bit
-
-SET LOCAL_PROGRAM_FILES=%ProgramFiles%
-GOTO :Continue
-
-:64Bit
-SET LOCAL_PROGRAM_FILES=%ProgramFiles(x86)%
-
-:Continue
-
-
 cd "%~dp0"
 "%LOCAL_PROGRAM_FILES%\SourceMonitor\SourceMonitor.exe" /s AddCheckpointAndExportMetrics.xml
 
--- build.bat
+++ build.bat
@@ -1,5 +1,21 @@
 @echo off
 
+IF DEFINED ProgramFiles(x86) GOTO :64Bit
+
+SET LOCAL_PROGRAM_FILES=%ProgramFiles%
+GOTO :Continue
+
+:64Bit
+SET LOCAL_PROGRAM_FILES=%ProgramFiles(x86)%
+
+:Continue
+
+set DEVENV="%LOCAL_PROGRAM_FILES%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+
+echo Building Solution
+cd "%~dp0"
+%DEVENV% TeamCityDemo.sln /Build Debug
+
 echo Running SourceMonitor analysis
 cd "%~dp0\BuildServer\SourceMonitor"
 call AddCheckpointAndOutputMetrics.bat
